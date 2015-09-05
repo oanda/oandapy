e51f139c477a1fa1114695da1ce28488f13cacb7
Feite Brekeveld <F.brekeveld@gmail.com>
2015-09-05 17:35:41 +0200

    make it pip installable

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='oandapy',
+      version=version,
+      description="Python wrapper for the OANDA REST API",
+      long_description="""\
+""",
+      classifiers=[
+            'Programming Language :: Python',
+            'License :: OSI Approved :: MIT License',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Financial and Insurance Industry'
+            'Operating System :: OS Independent',
+            'Development Status :: 5 - Production/Stable',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+      ], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='OANDA FOREX wrapper REST API',
+      author='OANDA',
+      author_email='api@oanda.com',
+      url='http://developer.oanda.com/',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'requests',
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
