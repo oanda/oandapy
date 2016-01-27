dd57a55ab639c92898c2029de026eea07b9002b2
hootnot <f.brekeveld@gmail.com>
2016-01-27 16:00:10 +0100

    added a NoHeartbeat test Streamer class modified (equal to the one in the test_streamer)

--- tests/test_streaming_rates_and_events.py
+++ tests/test_streaming_rates_and_events.py
@@ -15,22 +15,30 @@ class DisconnectException(Exception):
     pass
 
 
-class MyStream(oandapy.Streamer):
-
+class Stream(oandapy.Streamer):
+    """
+        class to parse the OANDA stream
+        tick records are processed into candles of different timeframes
+        candles that are ready are processed by the plugin manager
+    """
     def __init__(self, count=10, *args, **kwargs):
-        super(MyStream, self).__init__(*args, **kwargs)
+        super(Stream, self).__init__(*args, **kwargs)
         self.count = count
         self.reccnt = 0
+        self.hbcnt = 0
 
     def on_success(self, data):
         print data, "\n"
-        self.reccnt += 1
-        if self.reccnt == self.count:
+        if "heartbeat" in data:
+            self.hbcnt += 1
+        else:
+            self.reccnt += 1
+
+        if self.reccnt + self.hbcnt == self.count:
             self.disconnect()
 
     def on_error(self, data):
         self.disconnect()
-        # raise DisconnectException()
 
 
 class TestRates(unittest.TestCase):
@@ -38,6 +46,7 @@ class TestRates(unittest.TestCase):
     def setUp(self):
         global access_token
         global account
+        global api
 
         try:
             account, access_token = unittestsetup.auth()
@@ -45,19 +54,35 @@ class TestRates(unittest.TestCase):
             print("%s" % e)
             exit(0)
 
+        api = oandapy.API(environment=environment, access_token=access_token)
+
     def test__Rates(self):
+        """ get records from stream and including heartbeats,
+            #recs received should equal #recs requested + # hb recs
+        """
         count = 10
-        r = MyStream(access_token=access_token,
-                     environment=environment, count=count)
+        r = Stream(access_token=access_token,
+                   environment=environment, count=count)
         r.rates(account, instruments=",".join(instruments))
+        self.assertEqual(count, r.reccnt + r.hbcnt)
+
+    def test_RatesNoHeartBeats(self):
+        """ get records from stream and ignore heartbeats,
+            #recs received should equal #recs requested
+        """
+        count = 100
+        r = Stream(access_token=access_token, environment=environment,
+                   count=count)
+        r.rates(account, ignore_heartbeat=True,
+                instruments=",".join(instruments))
         self.assertEqual(count, r.reccnt)
 
     def test__Events(self):
         count = 4
-        r = MyStream(access_token=access_token,
-                     environment=environment, count=count)
+        r = Stream(access_token=access_token,
+                   environment=environment, count=count)
         r.events(ignore_heartbeat=False)
-        self.assertEqual(count, r.reccnt)
+        self.assertEqual(count, r.reccnt + r.hbcnt)
 
 
 if __name__ == "__main__":
