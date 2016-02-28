d1e22d5cb7be84d58a8bb088aae138164f3b09f0
hootnot <f.brekeveld@gmail.com>
2016-02-28 19:32:05 +0100

    pass the environment, construct the message

--- oandapy/exceptions.py
+++ oandapy/exceptions.py
@@ -2,5 +2,6 @@
 
 class BadEnvironment(Exception):
     """environment should be: sandbox, practice or live."""
-    def __init__(self, msg):
+    def __init__(self, environment):
+        msg = "Environment '%s' does not exist" % environment
         super(BadEnvironment, self).__init__(msg)
